fc557f96a2dd3244329f67d36044788de00fc389
Juan Carlos Mendoza <juan.mendoza@jmcxpress.com>
2019-09-07 19:41:14 -0500

    mongo db connection

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 'use strict';
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const express = require('express');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-var cors = require('cors');
+const app = express();
 
-var app = express();
+// Basic Configuration
+const port = process.env.PORT || 3000;
 
-// Basic Configuration 
-var port = process.env.PORT || 3000;
+/** this project needs a db !! **/
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI, { useMongoClient: true }, function(err) {
+  if (err) {
+    console.log('Error to connect to DB', err);
+  } else {
+    console.log('DB successfuly connected');
+  }
+});
 
 app.use(cors());
 
@@ -21,17 +27,15 @@ app.use(cors());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
-
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Node.js listening ...');
 });
