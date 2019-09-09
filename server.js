3912befbc2b389ada7674865004d26b4bd25cd91
Juan Carlos Mendoza <juan.mendoza@jmcxpress.com>
2019-09-09 11:51:16 -0500

    dns.lookup

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/
-mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true }, function (err) {
+mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true }, function(err) {
   if (err) {
-    console.log('DB Error connection')
+    console.log('DB Error connection');
   } else {
-    console.log('Db successfully connected')
+    console.log('Db successfully connected');
   }
 });
 const Schema = mongoose.Schema;
@@ -25,9 +25,9 @@ const urlSchema = new Schema({
   original_url: String
 });
 
-urlSchema.plugin(AutoIncrement, {inc_field: 'id'});
+urlSchema.plugin(AutoIncrement, { inc_field: 'id' });
 
-const Url = mongoose.model('urls', urlSchema)
+const Url = mongoose.model('urls', urlSchema);
 
 app.use(cors());
 
@@ -38,29 +38,32 @@ app.use(bodyParser.json());
 
 // Project challenge
 app.post('/api/shorturl/new', function(req, res) {
-  const original_url = req.body.url
-  const newUrl = new Url({ original_url })
-  newUrl.save(function(err, data) {
-    if (err) return res.json({error: "Erro saving data"})
-    return res.json({
-      short_url: newUrl.id,
-      original_url
-    })
-  })
-})
+  const url = req.body.url;
+  dns.lookup(url, function(arr, address) {
+    if (err) return res.json({ error: 'invalid URL' });
+    const newUrl = new Url({ original_url: url });
+    newUrl.save(function(err, data) {
+      if (err) return res.json({ error: 'Error saving data' });
+      return res.json({
+        short_url: newUrl.id,
+        original_url: url
+      });
+    });
+  });
+});
 
 app.get('/api/shorturl', async function(req, res) {
-  const urlList = await Url.find()
-  res.json({urlList})
-})
+  const urlList = await Url.find();
+  res.json({ urlList });
+});
 
 app.get('/api/shorturl/:id', function(req, res) {
-  const id = req.params.id
-  Url.find({ id }, function (err, data) {
-    if (err) return res.json({ error: "error finding url"})
-    return res.json({ data })
-  })
-})
+  const id = req.params.id;
+  Url.find({ id }, function(err, data) {
+    if (err) return res.json({ error: 'error finding url' });
+    return res.json({ data });
+  });
+});
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -74,5 +77,5 @@ app.get('/api/hello', function(req, res) {
 });
 
 app.listen(port, function() {
-  console.log('Node.js listening ...');
+  console.log('Node.js listening on port', port);
 });
